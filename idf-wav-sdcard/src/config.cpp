86807ff33a3eb6c7652ab80b06f9627e83520c4b
Tri Nguyen <nnt@leapslabs.com>
2022-12-13 13:38:43 +0700

    config: Changed pins to match eloc project and use apll for i2s

--- idf-wav-sdcard/src/config.cpp
+++ idf-wav-sdcard/src/config.cpp
@@ -22,18 +22,20 @@ i2s_config_t i2s_mic_Config = {
     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
     .communication_format = I2S_COMM_FORMAT_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 4,
+    .dma_buf_count = 16,
     .dma_buf_len = 1024,
-    .use_apll = false,
+    .use_apll = true,
     .tx_desc_auto_clear = false,
     .fixed_mclk = 0};
 
 // i2s microphone pins
 i2s_pin_config_t i2s_mic_pins = {
+    .mck_io_num = I2S_PIN_NO_CHANGE,
     .bck_io_num = I2S_MIC_SERIAL_CLOCK,
     .ws_io_num = I2S_MIC_LEFT_RIGHT_CLOCK,
     .data_out_num = I2S_PIN_NO_CHANGE,
-    .data_in_num = I2S_MIC_SERIAL_DATA};
+    .data_in_num = I2S_MIC_SERIAL_DATA
+};
 
 // i2s speaker pins
 i2s_pin_config_t i2s_speaker_pins = {
--- idf-wav-sdcard/src/config.h
+++ idf-wav-sdcard/src/config.h
@@ -2,7 +2,7 @@
 #include <driver/i2s.h>
 
 // save to SPIFFS instead of SD Card?
-#define USE_SPIFFS 1
+// #define USE_SPIFFS 1
 
 // sample rate for the system
 #define SAMPLE_RATE 16000
@@ -15,9 +15,9 @@
 // Generally they will default to LEFT - but you may need to attach the L/R pin to GND
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
 // #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_26
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_22
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_21
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_14
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_12
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_27
 
 // Analog Microphone Settings - ADC1_CHANNEL_7 is GPIO35
 #define ADC_MIC_CHANNEL ADC1_CHANNEL_7
@@ -28,13 +28,13 @@
 #define I2S_SPEAKER_SERIAL_DATA GPIO_NUM_18
 
 // record button
-#define GPIO_BUTTON GPIO_NUM_23
+#define GPIO_BUTTON GPIO_NUM_0
 
 // sdcard
-#define PIN_NUM_MISO GPIO_NUM_4
-#define PIN_NUM_CLK GPIO_NUM_14
-#define PIN_NUM_MOSI GPIO_NUM_15
-#define PIN_NUM_CS GPIO_NUM_25
+#define PIN_NUM_MISO GPIO_NUM_19
+#define PIN_NUM_CLK GPIO_NUM_18
+#define PIN_NUM_MOSI GPIO_NUM_23
+#define PIN_NUM_CS GPIO_NUM_5
 
 // i2s config for using the internal ADC
 extern i2s_config_t i2s_adc_config;
